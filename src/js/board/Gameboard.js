1b08dbbc1a47ba75cd2f93a0a59b40b78de9e563
Danilo Casim Jr. <danilocasim174@gmail.com>
2025-07-13 01:10:13 +0800

    feat: change the board to 2D and place() to apply horizontal or vertical

--- src/js/board/Gameboard.js
+++ src/js/board/Gameboard.js
@@ -1,23 +1,41 @@
 export class Gameboard {
   board = [];
   constructor() {
-    for (let i = 0; i < 100; i++) {
-      this.board.push({ isHit: false });
+    for (let i = 0; i < 10; i++) {
+      this.board.push([]);
+      for (let j = 0; j < 10; j++) {
+        this.board[i].push({ isHit: false });
+      }
     }
   }
 
-  place(index, ship) {
-    this.board[index] = ship;
+  place(index, ship, position) {
+    const rowIndex = index[0];
+    const colIndex = index[1];
+    if (position === "horizontal") {
+      for (let i = 0; i < ship.length; i++) {
+        this.board[rowIndex][colIndex + i] = ship;
+      }
+    }
+    if (position === "vertical") {
+      for (let i = 0; i < ship.length; i++) {
+        this.board[rowIndex + i][colIndex] = ship;
+      }
+    }
   }
 
   receiveAttack(index) {
-    return this.board[index].length
-      ? this.board[index].hit()
-      : (this.board[index].isHit = true);
+    const rowIndex = index[0];
+    const colIndex = index[1];
+
+    return this.board[rowIndex][colIndex].length
+      ? this.board[rowIndex][colIndex].hit()
+      : (this.board[rowIndex][colIndex].isHit = true);
   }
 
   isAllShipSunk() {
-    const existingShips = this.board.filter((b) => b.length);
+    const existingShips = this.board.flat().filter((ship) => ship.length);
+
     return existingShips.every((ship) => ship.isSunk());
   }
 }
--- test/util/gameboard.test.js
+++ test/util/gameboard.test.js
@@ -2,39 +2,53 @@ import { Gameboard } from "../../src/js/board/Gameboard.js";
 import { Ship } from "../../src/js/entities/Ship.js";
 
 describe("Gameboard", () => {
-    it("Should place ships on specific coordinates", () => {
+     it ("Should place the ship horizontally and have the instance of the ship", ()=> {
         const gameboard = new Gameboard()
-        gameboard.place(33, new Ship(1))
+        gameboard.place([0,7], new Ship(3), "horizontal")
+        expect(gameboard.board[0][7] instanceof Ship).toBe(true)
+        expect(gameboard.board[0][8] instanceof Ship).toBe(true)
+        expect(gameboard.board[0][9] instanceof Ship).toBe(true)
 
-        expect(gameboard.board[33]).toEqual(new Ship(1))
+    })
+
+    it("Should place the ship vertically and have the instance of the ship", () => {
+        const gameboard = new Gameboard()
+        gameboard.place([0,7], new Ship(3), "vertical")
+        expect(gameboard.board[0][7] instanceof Ship).toBe(true)
+        expect(gameboard.board[1][7] instanceof Ship).toBe(true)
+        expect(gameboard.board[2][7] instanceof Ship).toBe(true)
     })
 
     it("Should receive an attack with ship then hit it", () => {
         const gameboard = new Gameboard()
-        gameboard.place(33, new Ship(2))
-        gameboard.receiveAttack(33)
-        expect(gameboard.board[33].numberOfHits).toBe(1)
+        gameboard.place([1,0], new Ship(2), "horizontal")
+        gameboard.receiveAttack([1,0])
+        expect(gameboard.board[1][0].numberOfHits).toBe(1)
+        expect(gameboard.board[1][1].numberOfHits).toBe(1)
+
     })
 
     it("Should receive an attack without ship then record the missed shot", () => {
         const gameboard = new Gameboard()
-        gameboard.place(33, new Ship(1))
-        gameboard.receiveAttack(32)
-        expect(gameboard.board[32].isHit).toBe(true)
+        gameboard.place([2,0], new Ship(1), "vertical")
+        gameboard.receiveAttack([1,0])
+        expect(gameboard.board[1][0].isHit).toBe(true)
     })
 
     it("Should return true if all ships is sunk", () => {
         const gameboard = new Gameboard()
-        gameboard.place(33, new Ship(2))
-        gameboard.receiveAttack(33)
-        gameboard.receiveAttack(33)
+        gameboard.place([2,0], new Ship(2), "horizontal")
+        gameboard.receiveAttack([2,0])
+        gameboard.receiveAttack([2,1])
         expect(gameboard.isAllShipSunk()).toBe(true)
 
     })
     it ("Should return false if all ship isn't sunk", () => {
         const gameboard = new Gameboard()
-        gameboard.place(33, new Ship(2))
-        gameboard.receiveAttack(33)
+        gameboard.place([1,0], new Ship(2), "vertical")
+        gameboard.receiveAttack([1,1])
         expect(gameboard.isAllShipSunk()).toBe(false)
     })
+
+  
 })
