c086ceb426bc078134efb4d51a93a71e9ecc8099
Danilo Casim Jr. <danilocasim174@gmail.com>
2025-07-13 23:46:45 +0800

    feat: intializing a pre position ships

--- src/js/index.js
+++ src/js/index.js
@@ -1,23 +1,22 @@
 import style from "../css/style.css";
-import { Human } from "./entities/Human.js";
-import { Ship } from "./entities/Ship.js";
+import { Human } from "./model/Human.js";
+import { Computer } from "./model/Computer.js";
+import { Ship } from "./model/Ship.js";
+import { renderBoard } from "./views/dom-render.js";
+import { attackComputer } from "./controllers/event.js";
 
 const humanPlayer = new Human();
 humanPlayer.gameboard.place([2, 2], new Ship(2), "horizontal");
 humanPlayer.gameboard.place([1, 1], new Ship(4), "vertical");
-// humanPlayer.gameboard.place([5, 5], new Ship(4), "horizontal");
+humanPlayer.gameboard.place([0, 9], new Ship(4), "vertical");
+humanPlayer.gameboard.place([9, 0], new Ship(3), "horizontal");
 
-// humanPlayer.gameboard.place([3, 3], new Ship(3), "horizontal");
+const computerPlayer = new Computer();
+computerPlayer.gameboard.place([4, 2], new Ship(2), "horizontal");
+computerPlayer.gameboard.place([1, 1], new Ship(4), "vertical");
+computerPlayer.gameboard.place([1, 9], new Ship(4), "vertical");
+computerPlayer.gameboard.place([9, 0], new Ship(3), "horizontal");
 
-// humanPlayer.gameboard.place([1, 5], new Ship(4), "horizontal");
+renderBoard(humanPlayer.gameboard.board, computerPlayer.gameboard.board);
 
-humanPlayer.gameboard.receiveAttack([2, 2]);
-humanPlayer.gameboard.receiveAttack([2, 3]);
-
-humanPlayer.gameboard.receiveAttack([1, 1]);
-humanPlayer.gameboard.receiveAttack([2, 1]);
-humanPlayer.gameboard.receiveAttack([3, 1]);
-humanPlayer.gameboard.receiveAttack([4, 1]);
-
-console.log(humanPlayer.gameboard.board);
-console.log(humanPlayer.gameboard.isAllShipSunk());
+attackComputer();
